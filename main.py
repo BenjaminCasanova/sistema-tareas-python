d6e4e5af256d510ead20188faadd85c08bc0d708
BenjaminCasanova <be.casanovav@duocuc.cl>
2026-06-23 18:25:42 -0400

    Update main.py
    Se implementa menu principal.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+def menu():
+  while True:
+    print("/n ---Menu---")
+    print("1. Agregar tarea")
+    print("2. Listar tareas")
+    print("3. Salir")
 
+    opcion = input("Seleccione una opcion: ")
+
+    if opcion == "1":
+            print("Función agregar tarea proximamente")
+        elif opcion == "2":
+            print("Función listar tareas proximamente")
+        elif opcion == "3":
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opción invalida")
+
+menu()
