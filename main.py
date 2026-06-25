49ec235675f7608ab871a45950eefce4c600b489
BenjaminCasanova <be.casanovav@duocuc.cl>
2026-06-24 22:34:54 -0400

    Update main.py
    se implementa registro y listado de tareas..

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
+# Lista donde se almaceranan las tareas
+tareas = []
+
+# Función para agregar una tarea
+def agregar_tarea():
+    nombre = input("Ingrese el nombre de la tarea: ")
+
+    if nombre.strip() == "":
+        print("El nombre de la tarea no puede estar vacío.")
+        return
+
+    tarea = {
+        "nombre": nombre,
+        "estado": "Pendiente"
+    }
+
+    tareas.append(tarea)
+    print("Tarea agregada correctamente.")
+
+# Función para listar las tareas
+def listar_tareas():
+    if len(tareas) == 0:
+        print("No hay tareas registradas.")
+        return
+
+    print("\n===== LISTA DE TAREAS =====")
+
+    for i in range(len(tareas)):
+        print(f"{i + 1}. {tareas[i]['nombre']} - {tareas[i]['estado']}")
+
+
 def menu():
-  while True:
-    print("/n ---Menu---")
-    print("1. Agregar tarea")
-    print("2. Listar tareas")
-    print("3. Salir")
 
-    opcion = input("Seleccione una opcion: ")
+    while True:
 
-    if opcion == "1":
-            print("Función agregar tarea proximamente")
+        print("\n===== SISTEMA DE REGISTRO DE TAREAS =====")
+        print("1. Agregar tarea")
+        print("2. Listar tareas")
+        print("3. Salir")
+
+        opcion = input("Seleccione una opcion: ")
+
+        if opcion == "1":
+            agregar_tarea()
+
         elif opcion == "2":
-            print("Función listar tareas proximamente")
+            listar_tareas()
+
         elif opcion == "3":
-            print("Saliendo del sistema...")
+            print("Gracias por utilizar el sistema.")
             break
+
         else:
-            print("Opción invalida")
+            print("Opcion invalida.")
+
 
 menu()
